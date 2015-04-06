d6e134c7e70f6da19ab5964cc211b74a01f59769
James Bell <james@james-bell.co.uk>
2015-04-06 18:17:29 +0100

    Removed test for now until I write one that works

--- test.js
+++ test.js
@@ -2,6 +2,6 @@
 var assert = require('assert');
 var postmarkSpamcheck = require('./');
 
-it('should ', function () {
-	assert.strictEqual(postmarkSpamcheck.check(), 'unicorns & rainbows');
-});
+// it('should ', function () {
+// 	assert.strictEqual(postmarkSpamcheck.check(), 'unicorns & rainbows');
+// });
